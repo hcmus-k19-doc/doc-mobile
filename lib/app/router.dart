d0ff20c3904ad3b7cd517cae6d87bd1a5292a4ac
Lam Thinh Phat <lamthinhphat2001@gmail.com>
2023-03-24 09:01:24 +0700

    [DOC-150]Fix using Java 17 temurin and Rename basescreen

--- lib/app/router.dart
+++ lib/app/router.dart
@@ -1,14 +1,13 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_app/ui/pages/base/base_screen.dart';
 import 'package:flutter_app/ui/pages/forgot_pass/check_your_mail_screen.dart';
 import 'package:flutter_app/ui/pages/forgot_pass/forgot_pass_screen.dart';
+import 'package:flutter_app/ui/pages/home/base_screen.dart';
 import 'package:flutter_app/ui/pages/login/login_screen.dart';
 import 'package:flutter_app/ui/pages/settings/settings_screen.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:path/path.dart';
 
-import '../ui/pages/home/home_screen.dart';
 import '../ui/pages/splash/splash_screen.dart';
 
 class HomeArguments {
@@ -25,12 +24,11 @@ class CameraArguments {
 }
 
 class MyRouter {
-  static const String home = 'home';
   static const String splash = '/splash';
   static const String setting = '/setting';
 
   //DOC
-  static const String baseScreen = '/base-screen';
+  static const String homeScreen = '/home-screen';
   static const String login = '/login';
   static const String forgotPassword = "/forgot-password";
   static const String checkYourMail = "/check-your-mail";
@@ -42,19 +40,8 @@ class MyRouter {
         return MaterialPageRoute(builder: (_) => const SplashScreen());
       // case home:
       //   return MaterialPageRoute(builder: (_) => HomeScreen(title: settings.arguments, number: null,));
-      case home:
-        if (args is HomeArguments) {
-          String mTitle = args.title;
-          int mNumber = args.number;
-          return MaterialPageRoute(
-              builder: (_) => HomeScreen(title: mTitle, number: mNumber),
-              settings:
-                  RouteSettings(name: home, arguments: settings.arguments));
-        } else {
-          return errorRoute("Input for Home - routing is not ScreenArguments");
-        }
-      case baseScreen:
-        return MaterialPageRoute(builder: (_) => const BaseScreen());
+      case homeScreen:
+        return MaterialPageRoute(builder: (_) => const HomeScreen());
       case login:
         return MaterialPageRoute(
           builder: (_) => LoginScreen(),
--- lib/ui/pages/splash/splash_screen.dart
+++ lib/ui/pages/splash/splash_screen.dart
@@ -45,7 +45,7 @@ class _SplashScreenState extends State<SplashScreen> {
         if (state is AuthError || state is UnAuthenticated) {
           Navigator.pushReplacementNamed(context, MyRouter.login);
         } else if (state is Authenticated) {
-          Navigator.pushReplacementNamed(context, MyRouter.baseScreen);
+          Navigator.pushReplacementNamed(context, MyRouter.homeScreen);
         }
       },
       child: Scaffold(
