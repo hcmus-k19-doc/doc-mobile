a110952ad9b1aaa920e6f9cb4a4a17be2341b2df
PhatSankar <117284890+PhatSankar@users.noreply.github.com>
2023-06-27 22:24:24 +0700

    [DOC-153] fix calendar reminder (#25)
    * [DOC-153]Fix calendar reminder
    
    * [DOC-153]Fix reminder tile

--- lib/ui/pages/reminder_calendar/widgets/reminder_tile.dart
+++ lib/ui/pages/reminder_calendar/widgets/reminder_tile.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/app/router.dart';
 import 'package:flutter_app/bloc/document_reminder_bloc/document_reminder_bloc.dart';
 import 'package:flutter_app/constants/export_constants.dart';
 import 'package:flutter_app/constants/style_const.dart';
 import 'package:flutter_app/model/incoming_document.dart';
 import 'package:flutter_app/model/reminder_detail.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class ReminderTile extends StatelessWidget {
   const ReminderTile({Key? key, required this.reminder}) : super(key: key);
@@ -12,40 +14,65 @@ class ReminderTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 8,
-      clipBehavior: Clip.hardEdge,
-      shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(StyleConst.defaultRadius15)),
-      child: Container(
-        margin: const EdgeInsets.symmetric(
-          vertical: StyleConst.defaultPadding8,
-        ),
-        padding: const EdgeInsets.symmetric(
-            horizontal: StyleConst.defaultPadding16,
-            vertical: StyleConst.defaultPadding12),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  reminder.incomingNumber ?? "",
-                  style: bodyLargeBold(context),
+    return InkWell(
+      onTap: () {
+        if (reminder.documentType == "OUTGOING_DOCUMENT") {
+          Navigator.of(context).pushNamed(MyRouter.outgoingDocumentDetail,
+              arguments:
+                  DocumentDetailArgs(documentId: reminder.documentId ?? -1));
+        } else if (reminder.documentType == "INCOMING_DOCUMENT") {
+          Navigator.of(context).pushNamed(MyRouter.incomingDocumentDetail,
+              arguments:
+                  DocumentDetailArgs(documentId: reminder.documentId ?? -1));
+        }
+      },
+      child: Card(
+        elevation: 8,
+        clipBehavior: Clip.hardEdge,
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(StyleConst.defaultRadius15)),
+        child: Container(
+          margin: const EdgeInsets.symmetric(
+            vertical: StyleConst.defaultPadding8,
+          ),
+          padding: const EdgeInsets.symmetric(
+              horizontal: StyleConst.defaultPadding16,
+              vertical: StyleConst.defaultPadding12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          "${AppLocalizations.of(context)!.documentTypeReminder(reminder.documentType ?? "")}: ",
+                          style: bodyLargeBold(context),
+                        ),
+                        Expanded(
+                          child: Text(
+                            reminder.documentName ?? "",
+                            style: bodyLargeBold(context),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: StyleConst.defaultPadding4,
+                    ),
+                    Text(
+                      reminder.summary ?? "",
+                      style: bodyLarge(context)?.copyWith(color: Colors.grey),
+                    ),
+                  ],
                 ),
-                const SizedBox(
-                  height: StyleConst.defaultPadding4,
-                ),
-                Text(
-                  reminder.summary ?? "",
-                  style: bodyLarge(context)?.copyWith(color: Colors.grey),
-                ),
-              ],
-            ),
-            Text(reminder.expirationDate ?? "",
-                style: bodyLarge(context)?.copyWith(color: Colors.grey))
-          ],
+              ),
+              Text(reminder.expirationDate ?? "",
+                  style: bodyLarge(context)?.copyWith(color: Colors.grey))
+            ],
+          ),
         ),
       ),
     );
